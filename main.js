378e408a2faff45d5b6f6d23a7a435c68fcfb0d7
appert <scoreur@gmail.com>
2022-05-24 14:42:13 +0200

    correction h2 et section

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+// Toggle : permettant de garder la navbar collée (sticky).
+
 window.addEventListener("scroll",function() {
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+// Toggle : actif sur le clic et la nav (active).
 
 const navigation = document.querySelector('nav');
 document.querySelector('.toggle').onclick = function (){
